fde14d3784962c43184a25f52d27258e5975a365
asual <rostislav@asual.com>
2010-03-19 23:16:34 +0200

    Polishing.

--- samples/crawling/index.php
+++ samples/crawling/index.php
@@ -1,25 +1,25 @@
 <?php 
-                
-    $ebits = ini_get('error_reporting');
-    error_reporting($ebits ^ (E_NOTICE | E_WARNING));
+
+    error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
     
     $fragment = $_REQUEST['_escaped_fragment_'];
     $file = 'data/' . (isset($fragment) && $fragment != '' && $fragment != '/' ? preg_replace('/\//', '', $fragment) : 'home') . '.xml';
+    $re = '/(^<[^>]*>)|(\n|\r\n|\t|\s{2,4})*/';
     
     $handle = fopen($file, 'r');
     if ($handle != false) {
-        $content = preg_replace('/(^<\?(?>!(\?>)).*?>)|(\n|\r\n|\t|    )*/', '', fread($handle, filesize($file)));
+        $content = preg_replace($re, '', fread($handle, filesize($file)));
         fclose($handle);
     } else {
     	$content = 'Page not found!';
         header(php_sapi_name() == 'cgi' ? 'Status: 404' : 'HTTP/1.1 404');
     }
-                    
+    
 ?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>jQuery Address Crawling</title>
+        <title>jQuery Address Ajax Crawling</title>
         <meta http-equiv="content-type" content="text/html; charset=utf-8">
         <link type="text/css" href="styles.css" rel="stylesheet">
         <script type="text/javascript" src="jquery-1.4.2.min.js"></script>
@@ -27,16 +27,14 @@
         <script type="text/javascript">
             
             $.address.init(function(event) {
-                
             	$('.nav a').address();
-            	
             }).change(function(event) {
-
                 $('.nav a').each(function() {
                     $(this).toggleClass('selected', $(this).attr('href') == '#!' + event.value);
                 });
-                
-                $('.content').load('data/' + (event.value == '/' ? 'home' : event.value) + '.xml');
+                $.get('data/' + (event.value == '/' ? 'home' : event.value) + '.xml', function(data) {
+                    $('.content').html(data.replace(<?php echo($re); ?>g, ''));
+                }, 'text');
             });
     
         </script>
