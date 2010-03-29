5b18a543822af86cc966a55d9a97d2a190787675
asual <rostislav@asual.com>
2010-03-29 09:55:01 +0300

    Updates the data source and logic.

--- samples/crawling/index.php
+++ samples/crawling/index.php
@@ -7,33 +7,39 @@
     } else {
         $fragment = $_REQUEST[FRAGMENT];
     }
+
+    $arr = explode('?', $fragment, 2);
+
+    if (count($arr) > 1) {
+        $fragment = $arr[0];
+        parse_str($arr[1], $params);
+    }
     
-    $xml = new SimpleXMLElement(file_get_contents('data.xml'));
-    $pages = $xml->xpath('//page');
+    $xml = new SimpleXMLElement(file_get_contents('data.xml'));    
     
 ?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>jQuery Address Ajax Crawling</title>
+        <title>jQuery Address Crawling</title>
         <meta http-equiv="content-type" content="text/html; charset=utf-8">
         <link type="text/css" href="styles.css" rel="stylesheet">
         <script type="text/javascript" src="jquery-1.4.2.min.js"></script>
-        <script type="text/javascript" src="jquery.address-1.2.min.js"></script>
+        <script type="text/javascript" src="jquery.address-1.2.min.js?crawlable=true"></script>
         <script type="text/javascript">
             
             $.address.init(function(event) {
                 $('.nav a').address();
             }).change(function(event) {
                 $('.nav a').each(function() {
-                    $(this).toggleClass('selected', $(this).attr('href') == '#!' + event.value);
+                    $(this).toggleClass('selected', $(this).attr('href') == '#!' + event.path);
                 });
                 $.get(location.pathname + '?<?php echo(FRAGMENT); ?>=' + event.value, function(data) {
                     $('.content')
                         .show()
-                        .html($('.abstract', data));
+                        .html($('.content', data).html());
                 });
-            }).crawlable(true);
+            });
 
             // Graceful FOUC
             document.write('<style type="text/css"> .content { display: none; } </style>');
@@ -42,21 +48,43 @@
     </head>
     <body>
         <div class="page">
-            <h1>jQuery Address Ajax Crawling</h1>
+            <h1>jQuery Address Crawling</h1>
             <ul class="nav">
                 <?php
-                    foreach ($pages as $page) {
+    
+				    $pages = $xml->xpath('//pages/page');
+                
+	                foreach ($pages as $page) {
                         echo('<li><a href="#!' . $page['href'] . '"' 
                             . ($fragment == $page['href'] ? ' class="selected"' : '') . '>' 
                             . $page['title'] . '</a></li>');
                     } 
+                
                 ?>
 
             </ul>
             <div class="content">
                 <?php
-                    $content = $xml->xpath('//page[@href="' . $fragment . '"]/div[@class="abstract"]');
-                    echo($content[0]->asXml());
+                
+                    $content = $xml->xpath('//pages/page[@href="' . $fragment . '"]');
+
+                    $more = !isset($params['more']) && count($content[0]->children()) > 1;
+
+                    foreach($content[0]->children() as $child) {
+                        echo $child->asXML();
+                    	if ($more) {
+                    		echo('<p><a href="#!' . $fragment . '?more=true">More</a></p>');
+                    		break;
+                    	}
+                    }
+                    
+                    if (isset($params['source']) && count($content[0]->children()) > 1) {
+                    	$source = $xml->xpath('//meta/source');
+						foreach($source[0]->children() as $child) {
+						    echo $child->asXML();
+						}
+                    }
+                    
                 ?>
             
             </div>
