21f75ccd2637b2a9cd147ff014622908d300db74
asual <rostislav@asual.com>
2010-03-31 18:07:49 +0300

    Improves the display of form parameters.

--- samples/form/form.php
+++ samples/form/form.php
@@ -1,1 +1,17 @@
-<?php print_r($_REQUEST); ?>
+<?php 
+    
+    function form($name) {
+    	if (isset($_REQUEST[$name])) {
+            echo($name . ': ' . $_REQUEST[$name] . PHP_EOL);
+    	}
+    }
+    
+    form('input');
+    form('textarea');
+    form('select');
+    form('checkbox1');
+    form('checkbox2');
+    form('checkbox3');
+    form('radio');
+    
+?>
