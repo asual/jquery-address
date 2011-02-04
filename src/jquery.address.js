b3fef83c2b5794517b0ddf7a86804781d92ee2e4
asual <rostislav@asual.com>
2011-02-04 12:34:34 +0200

    Fixes issue 53.

--- src/jquery.address.js
+++ src/jquery.address.js
@@ -211,7 +211,7 @@
                         _st(function() {
                             $(_frame).bind('load', function() {
                                 var win = _frame.contentWindow;
-                                _value = win[ID] !== UNDEFINED ? decode(win[ID]) : '';
+                                _value = win[ID] !== UNDEFINED ? $.address.decode(win[ID]) : '';
                                 if (_value != _href()) {
                                     _update(FALSE);
                                     _l.hash = _crawl(_value, TRUE);
